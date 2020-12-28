03b080a862275febebc703035fd4f1b18513b18d
Alex <alex.poulain@gmail.com>
2020-12-28 16:29:36 -0500

    Add files via upload
    - add hyperdrive code + fix for the train.py script.
    - add model persistence for the best run with joblib in the train.py script and in the notebook with best_run.download_file()

--- train.py
+++ train.py
@@ -10,22 +10,6 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
-
-ds = TabularDatasetFactory.from_delimited_files(path, validate=True, include_path=False, infer_column_types=True, set_column_types=None, separator=',', header=True, partition_format=None, support_multi_line=False, empty_as_string=False, encoding='utf8')
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 42)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -52,6 +36,23 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df #add missing return statement
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+
+ds = TabularDatasetFactory.from_delimited_files(path,separator=',', header=True)
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 42)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
@@ -69,6 +70,10 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+    
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/'+str(run.id)+'_model.joblib') #Model persistence
+
 
 if __name__ == '__main__':
     main()
